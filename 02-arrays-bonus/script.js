a6f07329d54f13d087df0814f429ded4295f9d5c
MauroBorzi <mauroborzi91@gmail.com>
2025-07-03 16:03:00 +0200

    task bonus 5

--- 02-arrays-bonus/script.js
+++ 02-arrays-bonus/script.js
@@ -40,4 +40,5 @@ else {
 }
 
 // 5. Unisci tutti gli insegnanti nell'array teachers in una stringa  separata da virgole e salvala nella variabile teachersString
-const teachersString = null;
+const teachersString = teachers.join(`, `);
+console.log(teachersString)
